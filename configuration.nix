3b05ca58354826fe0f456615a7de626b9acff96d
remimimimi <valent.xarin@gmail.com>
2021-04-26 21:49:31 +0300

    Update config

--- configuration.nix
+++ configuration.nix
@@ -2,27 +2,38 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, lib, pkgs, ... }:
+{ config, pkgs, ... }:
 
-let unstable = import <nixos-unstable> { allowUnfree = true; };
+let
+  unstableTarball =
+    fetchTarball
+      https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz;
 in {
-  imports = [ # Include the results of the hardware scan.
-    ./hardware-configuration.nix
-    ./cachix.nix
-  ];
+  # Allow unfree and unstable packages
+  nixpkgs.config = {
+    allowUnfree = true;
+    pulseaudio = true;
+    packageOverrides = pkgs: {
+      unstable = import unstableTarball {
+        config = config.nixpkgs.config;
+      };
+    };
+  };
 
-  hardware.opengl.driSupport32Bit = true;
-  hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
-  hardware.pulseaudio.support32Bit = true;
-
-  nixpkgs.overlays = [ (import ./nix-nerd-fonts-overlay/default.nix) ];
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "ScarletDevilMansion"; # Define your hostname.
+  networking.hostName = "Remimimimi"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Set your time zone.
+  time.timeZone = "Europe/Moscow";
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
@@ -37,93 +48,102 @@ in {
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
-    font = "FiraCode Nerd Font";
+    font = "Roboto-mono";
     keyMap = "us";
   };
 
-  # Set your time zone.
-  time.timeZone = "Europe/Moscow";
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.autorun = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
 
-  nixpkgs.config.allowUnfree = true;
+  # Desktop manager
+  services.xserver.windowManager.leftwm.enable = true;
 
-  fonts.fonts = with pkgs; [ nerd-fonts.firacode ];
+  # Configure keyboard settings in X11
+  services.xserver.layout = "us";
+  services.xserver.autoRepeatDelay = 210;
+  services.xserver.autoRepeatInterval = 40;
+  
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
 
-  services.picom = {
-    fade = true;
-    fadeSteps = [ "0.15" "0.15" ];
-    # shadow = true;
-    # shadowOffsets = [ (-10) (-10) ];
-    # shadowOpacity = "0.22";
-    # activeOpacity = "1.00";
-    # inactiveOpacity = "0.92";
-    settings = {
-      blur = true;
-      blur-background = true;
-      blur-background-frame = true;
-      blur-background-fixed = true;
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
 
-      blur-kern = "3x3box";
-      blur-method = "dual_kawase";
-      blur-strength = 10;
-      blur-background-exclude = [ "class_g = 'slop'" ];
-    };
+  hardware.enableAllFirmware = true;
+
+  # Setup docker
+  virtualisation.docker.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.remimimimi = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "video" "docker" ]; # Enable ‘sudo’ for the user.
+    home = "/home/remimimimi";
+    description = "Love low poly?";
+    shell = pkgs.zsh;
   };
 
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
     alacritty
     arc-icon-theme
     arc-theme
+    bat
     binutils
-    carnix
+    bibata-cursors
+    crate2nix
     clang
     cmake
     coreutils
+    spotify
     discord
+    dmenu
     dunst
-    ((emacsPackagesNgGen emacs).emacsWithPackages
-      (epkgs: [ epkgs.emacs-libvterm ]))
-    emojione
+    deno
+    exa
     fd
     feh
-    firefox-devedition-bin
-    fish
     flameshot
     font-awesome
+    firefox
+    noto-fonts-emoji
+    qutebrowser
     gcc
     git
     gnumake
+    gitAndTools.gh
     kakoune
     killall
     libpulseaudio
     libtool
     links2
-    links2
+    just
     neofetch
     nixfmt
     openssh
     openssl
     papirus-icon-theme
-    picom
     pass
+    rnix-lsp
+    roboto-mono
     ranger
     ripgrep
-    rustup
     sccache
     starship
     tdesktop
-    #unstable.emacs
     unstable.rust-analyzer
-    exa
     unzip
     wget
+    wmctrl
     xclip
   ];
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   wget vim
-  # ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -131,7 +151,6 @@ in {
   # programs.gnupg.agent = {
   #   enable = true;
   #   enableSSHSupport = true;
-  #   pinentryFlavor = "gnome3";
   # };
 
   # List services that you want to enable:
@@ -145,55 +164,13 @@ in {
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.layout = "us";
-  services.xserver.xkbOptions = "eurosign:e";
-  services.xserver.autorun = true;
-  services.xserver.videoDrivers = [ "nvidia" ];
-
-  # window manager
-  services.xserver.windowManager.i3.enable = true;
-  services.xserver.windowManager.i3.package = pkgs.i3-gaps;
-  services.xserver.windowManager.i3.extraPackages = with pkgs; [
-    dmenu
-    i3status
-    i3lock
-  ];
-
-  # Enable touchpad support.
-  # services.xserver.libinput.enable = true;
-
-  # Setup docker
-  virtualisation.docker.enable = true;
-
-  # Fish shell
-  programs.fish.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.remi = {
-    isNormalUser = true;
-    extraGroups =
-      [ "wheel" "networkmanager" "video" "docker" ]; # Enable ‘sudo’ for the user.
-    home = "/home/remi";
-    description = "Remilia Scarlet";
-    shell = pkgs.fish;
-  };
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.03"; # Did you read the comment?
+  system.stateVersion = "20.09"; # Did you read the comment?
 
 }
 
