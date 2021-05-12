22cd6c6645ab7cc65cebf5ee119198784d793b2d
remimimimi <valent.xarin@gmail.com>
2021-05-12 07:19:44 +0300

    Update nvim config in home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -209,8 +209,10 @@ in
               saga.init_lsp_saga {
                 error_sign = "",
                 warn_sign = "",
-                hint_sign = '',
-                infor_sign = '',
+                hint_sign = "",
+                infor_sign = "",
+                code_action_icon = "",
+                -- code_action_icon = " ",
               }
               EOF
 
